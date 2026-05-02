f1f936e6604ce0e1811bb8d0297d40b20bb4d156
camojica423 <camojica423@gmail.com>
2026-05-02 08:31:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hola, estoy empezando mi proyecto")
+class cliente:
+  def __init__(self, nombre, documento, correo):
+    if not nombre:
+      raise ValueError("el nombre no puede estar vacio")
+    if @ not in correo :
+      raise ValueError("correo invalido")
+
+    self.nombre= nombre
+    self.documento= documento
+    self.correo = correo
+
+# prueba
+try:
+    cliente1 = Cliente("Carlos", "123", "carlos@gmail.com")
+    print("Cliente creado correctamente")
+except Exception as e:
+    print("Error:", e)
