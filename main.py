9afad4fea63016ee05d9e210b88346a097e061cb
camojica423 <camojica423@gmail.com>
2026-05-03 08:58:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# funcion para guardar errores en logs.txt
 def guardar_log(mensaje):
     archivo = open("logs.txt", "a")
     archivo.write(mensaje + "\n")
     archivo.close()
-        
+
+
 from cliente import Cliente
 from servicios import ServicioSala, ServicioEquipo, ServicioAsesoria
 from reserva import Reserva
@@ -17,6 +19,7 @@ try:
     print("Reserva 1 OK - Total:", reserva1.calcular_total())
 except Exception as e:
     print("Error:", e)
+    guardar_log(str(e))
 
 try:
     cliente2 = Cliente("Ana", "456", "ana@gmail.com")
@@ -25,21 +28,45 @@ try:
     print("Reserva 2 OK - Total:", reserva2.calcular_total())
 except Exception as e:
     print("Error:", e)
+    guardar_log(str(e))
+
 
 print("---- PRUEBAS CON ERROR ----")
 
+# nombre vacío
 try:
     cliente3 = Cliente("", "111", "correo@gmail.com")
 except Exception as e:
     print("Error esperado:", e)
+    guardar_log(str(e))
 
+# correo inválido
 try:
     cliente4 = Cliente("Pedro", "222", "correo")
 except Exception as e:
     print("Error esperado:", e)
+    guardar_log(str(e))
 
+# duración inválida
 try:
     servicio3 = ServicioSala(2)
     reserva3 = Reserva(cliente1, servicio3, 0)
 except Exception as e:
     print("Error esperado:", e)
+    guardar_log(str(e))
+
+# servicio equipo negativo
+try:
+    servicio4 = ServicioEquipo(-1)
+    print(servicio4.calcular_precio())
+except Exception as e:
+    print("Error esperado:", e)
+    guardar_log(str(e))
+
+# asesoría negativa
+try:
+    servicio5 = ServicioAsesoria(-2)
+    print(servicio5.calcular_precio())
+except Exception as e:
+    print("Error esperado:", e)
+    guardar_log(str(e))
