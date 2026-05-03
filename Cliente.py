5ea0adf90263c82269bf10bb498d31fc010c4bb3
camojica423 <camojica423@gmail.com>
2026-05-03 08:29:44 -0500

    Update Cliente.py

--- Cliente.py
+++ Cliente.py
@@ -1,1 +1,10 @@
+class Cliente:
+    def __init__(self, nombre, documento, correo):
+        if not nombre:
+            raise ValueError("el nombre no puede estar vacio")
+        if "@" not in correo:
+            raise ValueError("correo invalido")
 
+        self.nombre = nombre
+        self.documento = documento
+        self.correo = correo
