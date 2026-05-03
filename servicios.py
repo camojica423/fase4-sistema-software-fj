496ba87b5973ae38fe2aeeae350f49fa2468896f
camojica423 <camojica423@gmail.com>
2026-05-03 09:17:30 -0500

    Update servicios.py

--- servicios.py
+++ servicios.py
@@ -1,45 +1,39 @@
-class servicio:
-  def __int__(self, nombre):
-    self.nombre = nombre
-    
-  def calcular_precio(self):
-    raise NotImplementedError("Este metodo debe ser implementado")
+class Servicio:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def calcular_precio(self):
+        raise NotImplementedError("Este metodo debe ser implementado")
+
 
 class ServicioSala(Servicio):
     def __init__(self, horas):
+        if horas <= 0:
+            raise ValueError("Las horas deben ser mayores a cero")
         super().__init__("Sala")
         self.horas = horas
-      
-  def calcular_precio(self):
-    return self.horas * 50
 
-from servicio import ServicioSala
+    def calcular_precio(self):
+        return self.horas * 50
 
-servicio1 = ServicioSala(2)
-print(servicio1.calcular_precio())
 
 class ServicioEquipo(Servicio):
     def __init__(self, dias):
+        if dias <= 0:
+            raise ValueError("Los dias deben ser mayores a cero")
         super().__init__("Equipo")
         self.dias = dias
 
-  def calcular_precio(self):
-    return self.dias * 30
+    def calcular_precio(self):
+        return self.dias * 30
 
-from servicio import ServicioEquipo
-
-servicio2 = ServicioEquipo(3)
-print("Precio equipo:", servicio2.calcular_precio())
 
 class ServicioAsesoria(Servicio):
     def __init__(self, sesiones):
+        if sesiones <= 0:
+            raise ValueError("Las sesiones deben ser mayores a cero")
         super().__init__("Asesoria")
         self.sesiones = sesiones
 
     def calcular_precio(self):
         return self.sesiones * 100
-
-  from servicio import ServicioAsesoria
-
-servicio3 = ServicioAsesoria(1)
-print("Precio asesoria:", servicio3.calcular_precio())
