243950701ab171df22b090bcb7400910569c1c28
camojica423 <camojica423@gmail.com>
2026-05-03 08:46:34 -0500

    Update servicios.py

--- servicios.py
+++ servicios.py
@@ -17,3 +17,29 @@ from servicio import ServicioSala
 
 servicio1 = ServicioSala(2)
 print(servicio1.calcular_precio())
+
+class ServicioEquipo(Servicio):
+    def __init__(self, dias):
+        super().__init__("Equipo")
+        self.dias = dias
+
+  def calcular_precio(self):
+    return self.dias * 30
+
+from servicio import ServicioEquipo
+
+servicio2 = ServicioEquipo(3)
+print("Precio equipo:", servicio2.calcular_precio())
+
+class ServicioAsesoria(Servicio):
+    def __init__(self, sesiones):
+        super().__init__("Asesoria")
+        self.sesiones = sesiones
+
+    def calcular_precio(self):
+        return self.sesiones * 100
+
+  from servicio import ServicioAsesoria
+
+servicio3 = ServicioAsesoria(1)
+print("Precio asesoria:", servicio3.calcular_precio())
