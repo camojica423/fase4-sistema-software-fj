93366b4fba7a85f64c00aa11509c0ef165ee3bb7
camojica423 <camojica423@gmail.com>
2026-05-03 08:52:19 -0500

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from cliente import cliente
+from cliente import Cliente
 
 try:
-  cliente1 = cliente("carlos", "123", "carlos@gail.com")
+  cliente1 = Cliente("carlos", "123", "carlos@gail.com")
   print:("cliente creado correctamente")
 except exeption as e:
   print("error", e)
