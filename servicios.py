b0cb43919d55453c66c461f35a3a31848d74f062
camojica423 <camojica423@gmail.com>
2026-05-03 08:45:28 -0500

    Update servicios.py

--- servicios.py
+++ servicios.py
@@ -1,1 +1,19 @@
+class servicio:
+  def __int__(self, nombre):
+    self.nombre = nombre
+    
+  def calcular_precio(self):
+    raise NotImplementedError("Este metodo debe ser implementado")
 
+class ServicioSala(Servicio):
+    def __init__(self, horas):
+        super().__init__("Sala")
+        self.horas = horas
+
+  def calcular_precio(self):
+    return self.horas * 50
+
+from servicio import ServicioSala
+
+servicio1 = ServicioSala(2)
+print(servicio1.calcular_precio())
